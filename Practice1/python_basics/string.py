75cecd52826a11fb8891b4f8a2df5df0b4426465
Asekeha <a_eslyambek@kbtu.kz>
2026-01-29 15:40:57 +0500

    Create string.py

---
+++ Practice1/python_basics/string.py
@@ -0,0 +1,8 @@
+#Strings in python are surrounded by either single quotation marks, or double quotation marks.
+#'hello' is the same as "hello".
+
+#for exaple:
+x="hello "
+y='world'
+print(x,y)
+#result:hello world
