50995a2d55f0823ecf940202779826caa835c2c9
Asekeha <a_eslyambek@kbtu.kz>
2026-01-29 15:32:00 +0500

    Create get_started.py

---
+++ Practice1/python_basics/get_started.py
@@ -0,0 +1,7 @@
+#Python is an interpreted programming language, this means that as a developer you write Python (.py) files in a text editor and then put those files into the python interpreter to be executed.
+#Let's write our first Python file, called hello.py, which can be done in any text editor:
+# hello.py:
+print("Hello, Kazakhstan")
+
+#The output should be:
+#Hello, World!
