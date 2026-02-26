351a89971b5ce071a7a65676397b3a5388ac4c79
Asekeha <a_eslyambek@kbtu.kz>
2026-02-26 13:22:44 +0500

    Create math.py

---
+++ Practice4/math.py
@@ -0,0 +1,46 @@
+# Math and random operations
+x = min(5, 10, 25)
+y = max(5, 10, 25)
+
+print(x)
+print(y)
+
+#2
+
+lenght = float(input())
+height = float(input())
+
+area = lenght * height
+print(area)
+
+#3
+
+import math
+
+x = math.sqrt(64)
+
+print(x)
+
+#4
+
+#Import math library
+import math
+
+#Round a number upward to its nearest integer
+x = math.ceil(1.4)
+
+#Round a number downward to its nearest integer
+y = math.floor(1.4)
+
+print(x)
+print(y)
+
+#5
+
+import math
+height = int(input())
+a = float(input())
+b = float(input())
+
+area = (a+b)/2 * height
+print(area)
